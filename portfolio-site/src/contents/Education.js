c3fc33a6e94c28b8b59ad17e69899e490addf3c7
candidate.Brantley.Wyche.ext <bxw5869@rit.edu>
2020-11-27 19:31:01 -0500

    Added cards to education page

--- portfolio-site/src/contents/Education.js
+++ portfolio-site/src/contents/Education.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import Widecard from '../components/Widecard';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Education extends Component{
 
@@ -8,12 +11,37 @@ class Education extends Component{
         return(
 
             <div className="condiv">
-
                 <h1 className="subtopic">My Education</h1>
+                <Container>
+                    <Row>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={require("../img/rit.jpg")} />
+                            <Card.Header as="h5">Bachelor of Science
+                                <Card.Subtitle className="mb-2 text-muted cardspace">Rochester Institute of Technology</Card.Subtitle>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    August 2017 - Present
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
 
-                <Widecard title="Bachelor of Science" where="Rochester Institute of Technology" from="August 2017" to="Present"/>
+                        <Col sm={2}></Col>
 
-                <Widecard title="Graduate" where="Seton Hall Preparatory School" from="September 2013" to="June 2017"/>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={require("../img/shp.png")} className="education-img"/>
+                            <Card.Header as="h5">High School Graduate
+                                <Card.Subtitle className="mb-2 text-muted cardspace">Seton Hall Preparatory School</Card.Subtitle>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    September 2013 - June 2017
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                </Container>
+                
 
             </div>
         )
